fb73a11ae1535bccf9f6a6099899713d9774cb6b
Nick Pongratz <nicholau@cs.wisc.edu>
2016-06-21 13:03:41 -0500

    Initial commit
    - Replaced md5 with sha256 as required by homebrew
    - Use https URLs instead of http

---
+++ sshpass.rb
@@ -0,0 +1,26 @@
+# To use, download this file (sshpass.rb) and run brew install:
+#   $ brew install ./sshpass.rb
+
+# History:
+#  Originally cloned from https://raw.githubusercontent.com/eugeneoden/homebrew/eca9de1/Library/Formula/sshpass.rb
+#  Update on 20160621 by Nick Pongratz:
+#    - Replaced md5 with sha256 as required by homebrew
+#    - Use https URLs instead of http
+
+require 'formula'
+
+class Sshpass < Formula
+  url 'https://sourceforge.net/projects/sshpass/files/sshpass/1.05/sshpass-1.05.tar.gz'
+  homepage 'https://sourceforge.net/projects/sshpass'
+  sha256 'c3f78752a68a0c3f62efb3332cceea0c8a1f04f7cf6b46e00ec0c3000bc8483e'
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make install"
+  end
+
+  def test
+    system "sshpass"
+  end
+end
